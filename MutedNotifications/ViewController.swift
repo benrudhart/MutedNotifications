18b971f6c38fb7a985997fb1d72e89da2979cafc
Ben Rudhart <hi@benrudhart.com>
2019-05-06 09:07:59 +0200

    Initial Commit

---
+++ MutedNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MutedNotifications
+//
+//  Created by Ben Rudhart on 06.05.19.
+//  Copyright © 2019 Ben Rudhart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
